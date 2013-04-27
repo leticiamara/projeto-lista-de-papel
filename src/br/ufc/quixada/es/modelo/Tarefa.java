1ea02f682a31c1abae3778d7ffe5f8d6268de602
suelhy91 <suelhy91@530d8eda-85dc-f105-8f0f-f9a55899ab6a>
2013-04-27 21:24:42 +0000

    O CRUD do sistema foi criado. Foi adicionado a biblioteca do Hibernate.

---
+++ src/br/ufc/quixada/es/modelo/Tarefa.java
@@ -0,0 +1,45 @@
+package br.ufc.quixada.es.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Tarefa {
+
+	@Id
+	@GeneratedValue
+	private long idTarefa;
+	
+	@Column
+	private String nome;
+	private String status;
+	
+	public void setIdTarefa(long idTarefa) {
+		this.idTarefa = idTarefa;
+	}
+	public long getIdTarefa() {
+		return idTarefa;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getStatus() {
+		return status;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/br/ufc/quixada/es/persistencia/ClassePrincipal.java
@@ -0,0 +1,43 @@
+package br.ufc.quixada.es.persistencia;
+
+import java.util.List;
+
+import br.ufc.quixada.es.DAOs.TarefaDAO;
+import br.ufc.quixada.es.modelo.Tarefa;
+
+
+
+public class ClassePrincipal {
+
+
+	public static void main(String[] args) {
+		
+		CriarTabelas.prepararSessao();
+		CriarTabelas.reiniciaBanco();
+		
+		//Cadastrar uma tarefa
+		TarefaDAO dao = new TarefaDAO();
+		
+		Tarefa t = new Tarefa();
+		t.setNome("Fazer questoes de concorrencia"); 
+		t.setStatus("to");
+		
+		
+		dao.insert(t);
+		
+		
+		
+//		List<Tarefa> tarefas = dao.selectTo();
+//		
+//		for (Tarefa ta : tarefas)
+//		{
+//			System.out.println(ta.getNome());
+//		}
+		
+		
+		
+
+		
+		
+}
+}
