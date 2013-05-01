a0a2e4cba5290e540712f5558d76be5f8b0a1667
suelhy91 <suelhy91@530d8eda-85dc-f105-8f0f-f9a55899ab6a>
2013-05-01 22:34:41 +0000

    Tabela Usuario criada no banco. Foi feito o CRUD e o mapeamento.

--- trunk/src/br/ufc/quixada/es/persistencia/ClassePrincipal.java
+++ trunk/src/br/ufc/quixada/es/persistencia/ClassePrincipal.java
@@ -3,7 +3,9 @@ package br.ufc.quixada.es.persistencia;
 import java.util.List;
 
 import br.ufc.quixada.es.DAOs.TarefaDAO;
+import br.ufc.quixada.es.DAOs.UsuarioDAO;
 import br.ufc.quixada.es.modelo.Tarefa;
+import br.ufc.quixada.es.modelo.Usuario;
 
 
 
@@ -16,13 +18,19 @@ public class ClassePrincipal {
 		CriarTabelas.reiniciaBanco();
 		
 		//Cadastrar uma tarefa
-		TarefaDAO dao = new TarefaDAO();
-		
+		UsuarioDAO daoU = new UsuarioDAO();
+		TarefaDAO dao = new TarefaDAO();		
+				 		
+		Usuario u = new Usuario();
+		u.setNome("suelhy");
+		u.setEmail("suelhy91@gmail.com");
+		u.setSenha("1234");
 		Tarefa t = new Tarefa();
-		t.setNome("Fazer questoes de concorrencia"); 
+		t.setNome("Fazer questoes de concorrencia");
+		t.setUsuario(u);
 		//t.setStatus("to");
 		
-		
+		daoU.insert(u);
 		dao.insert(t);
 		
 		
