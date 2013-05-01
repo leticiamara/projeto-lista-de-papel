586d53a30b7b239373b304d9bff92e7d7fccb1dc
rodrigogirao7 <rodrigogirao7@530d8eda-85dc-f105-8f0f-f9a55899ab6a>
2013-05-01 20:31:55 +0000

    Criada a branch para corrigir o erro do estouro de clientes no banco

---
+++ branches/Correcao_ConexaoBanco_1.0/WebContent/javascripts/tarefas.js
@@ -0,0 +1,133 @@
+$(function() {
+
+	var server = "http://localhost:8080/ListaDePapel";
+	var $lastClicked;
+
+	//remover item quando clicar na lixeira
+	function onTarefaDeleteClick() {
+		$(this).parent('.tarefa-item')
+		.off('click')
+		.hide('slow', function() {
+			
+			$this = $(this);
+			$.post(server + "/DeletarTarefa",
+			{id: $this.children(".tarefa-id").text()});
+			$(this).remove();
+		});
+	}
+
+	//recebe um texto e adiciona a tarefa à lista.
+	function addTarefa(text,id) {
+		//verifica se o id nao é undefined
+		id = id || 0;
+
+		var $tarefa = $("<div />")
+		.addClass("tarefa-item")
+		.append($("<div />")
+			.addClass("tarefa-id")
+			.text(id))
+		.append($("<div />")
+			.addClass("tarefa-text")
+			.text(text))
+		.append($("<div />")
+			.addClass("tarefa-delete"))
+		.append($("<div />")
+			.addClass("clear"));
+		$("#tarefa-list").append($tarefa);
+		$(".tarefa-delete").click(onTarefaDeleteClick);
+		$(".tarefa-item").click(onTarefaItemClick);
+		
+		if(id === 0) {
+			var div = $($tarefa.children(".tarefa-id"));
+			console.log("id", div);
+			novaTarefa(text, $(div));
+		}
+	}
+
+	//incluir nova tarefa quando apertar ENTER
+	function onTarefaKeydown(event) {
+		if(event.which === 13) {
+			addTarefa($("#tarefa").val());
+			$("#tarefa").val("");
+		}
+	}
+
+	//editar tarefa quando apertar ENTER
+	function onTarefaEditKeydown(event) {
+		if(event.which === 13) {
+			savePendingEdition($lastClicked);
+			$lastClicked = undefined;
+		}
+	}
+
+	//permite a alteração quando clicar numa tarefa
+	function onTarefaItemClick() {
+		if(!$(this).is($lastClicked)) {
+			if($lastClicked !== undefined) {
+				savePendingEdition($lastClicked);
+			}
+			$lastClicked = $(this);
+			var text = $lastClicked.children('.tarefa-text').text();
+			var id = $lastClicked.children('.tarefa-id').text();
+			console.log("onTarefaClick  id: ", id);
+			
+			var content = "<input type='text' class='tarefa-edit' value='" +
+			text + "'>" + "<div class='tarefa-id'>" + id + "</div>\"";
+			$lastClicked.html(content);
+			$(".tarefa-edit").keydown(onTarefaEditKeydown);
+		}
+	}
+
+	//salvar edição das tarefas
+	function savePendingEdition($tarefa) {
+		var id = $tarefa.children('.tarefa-id').text();
+		var text = $tarefa.children('.tarefa-edit').val();
+		
+		$tarefa.empty();
+		$tarefa.append("<div class='tarefa-id'>" + id + "</div>")
+		.append("<div class='tarefa-text'>" + text + "</div>")
+		.append("<div class='tarefa-delete'></div>")
+		.append("<div class='clear'></div>");
+		console.log("text: ",text);
+		console.log("id: ",id);
+		atualizarTarefa(text, id);
+		
+		$(".tarefa-delete").click(onTarefaDeleteClick);
+		$tarefa.click(onTarefaItemClick);
+	}
+
+	function carregarTarefas() {
+		$("#tarefa").empty();
+		$.getJSON(server + "/CarregarTarefas")
+		.done(function(data) {
+		console.log("data: ", data);
+		for(var tarefa = 0; tarefa < data.length; tarefa++) {
+			console.log("ENTROU NO FOR", tarefa);
+		addTarefa(data[tarefa].nome,data[tarefa].idTarefa);
+		}
+		});
+	}
+	
+	//Atualizar Tarefa no banco
+	function atualizarTarefa(text, id) {
+		$.post(server + "/AtualizarTarefa", {tarefa_id: id, texto: text});
+	}
+	
+	//Adicionar Tarefa no banco
+	function novaTarefa(text, $div) {
+		$.post(server + "/AdiconarTarefa",
+				{nome: text})
+				.done(function(data) {
+					$div.text(data);
+				});
+	}
+
+	//callbacks dos eventos
+	$(".tarefa-delete").click(onTarefaDeleteClick);
+	$(".tarefa-item").click(onTarefaItemClick);
+	$("#tarefa").keydown(onTarefaKeydown);
+	carregarTarefas();
+
+
+
+});
