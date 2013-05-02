4b73274db437068d6919d73a6d5ff1adb513c38d
rodrigogirao7 <rodrigogirao7@530d8eda-85dc-f105-8f0f-f9a55899ab6a>
2013-05-02 07:28:08 +0000

    Marcar tarefa como concluida e alteração no CSS do projeto.

--- src/br/ufc/quixada/es/servlet/ConcluirTarefa.java
+++ src/br/ufc/quixada/es/servlet/ConcluirTarefa.java
@@ -1,6 +1,8 @@
 package br.ufc.quixada.es.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,6 +35,8 @@ public class ConcluirTarefa extends HttpServlet {
 			tarefa.setStatus("to");
 			daoTarefa.update(tarefa);
 		}
+		PrintWriter out = response.getWriter();
+		out.write(""+tarefa.getStatus());
 
 	}
 
